c36cc379e5677e69f30860b84b2e439a04b3cd36
adisakshya <adisakshya98@gmail.com>
2021-06-27 14:42:20 +0530

    updated powershell profile

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -1,5 +1,16 @@
-# Path - C:\Users\Acer\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
+# Import modules
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module PSReadLine
+
+# Autocomplete
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Intellisense
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -HistoryNoDuplicates
+
+# Set Oh My Posh prompt
 Set-PoshPrompt -Theme ~/adisakshya.yaml
